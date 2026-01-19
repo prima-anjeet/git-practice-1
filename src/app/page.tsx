2a1ed6d22ae62028377b96d1c841884d6e2eaafc
Anjeet Singh <company-anjeet.singh@primathon.in>
2026-01-19 16:39:44 +0530

    3 link add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="p-4">hello bro</div>
        <a href="https://primathon.in" className="bg-red-400 text-blue">primathon</a>
+       <a href="https://primathon.in" className="bg-red-400 text-blue">primathon</a>
+       <a href="https://primathon.in" className="bg-red-400 text-blue">primathon</a>
     </div>
   );
 }
