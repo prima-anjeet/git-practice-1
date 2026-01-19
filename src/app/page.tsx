6855dfc31b4ff3ffbb975473b2ed66e0dfb74e40
Anjeet Singh <company-anjeet.singh@primathon.in>
2026-01-19 15:51:41 +0530

    add lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <div className="p-4">hello bro</div>
+      <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio corrupti consequuntur eos ratione illum, cupiditate recusandae animi ab dolores minima aut, pariatur excepturi dolor adipisci earum assumenda nesciunt est architecto tempora necessitatibus neque temporibus maiores! Tempora numquam nam qui quam?</p></div>
     </div>
   );
 }
